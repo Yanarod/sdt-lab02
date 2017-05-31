adcc23b8601c52cd4294a5c8b344d6ae74fecdb3
Яна Родионова <yanarod98@mpei.ru>
2017-06-01 00:31:43 +0300

    umnozhenie_na_2

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-int a, b;
-cout << "Enter A and B: ";
-cin >> a >> b;
+    int a, b;
+    cout << "Enter A and B: ";
+    cin >> a >> b;
     cout << "A + B = " << a + b << '\n';
+    cout << "A * 2 = " << a*2 << '\n';
 }
